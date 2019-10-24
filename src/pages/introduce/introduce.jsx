eec85b2dc8fe35d011ab3c64ff64b204507b35bf
yechuanjie <1102684711@qq.com>
2019-10-24 17:01:37 +0800

    使用远程地址

--- src/pages/introduce/introduce.jsx
+++ src/pages/introduce/introduce.jsx
@@ -7,12 +7,13 @@ import './introduce.scss';
 // );
 // const images = requireContext.keys().map(requireContext);
 const imageList = () => {
-    const images = [];
-    for (let i = 0; i <= 10; i++) {
-      const e = require(`../../assets/introduce/${i}.png`);
-      images.push(e);
-    }
-    return images;
+  const images = [];
+  for (let i = 0; i <= 10; i++) {
+    //   const e = require(`../../assets/introduce/${i}.png`);
+    const e = `http://pzpsn4agr.bkt.clouddn.com/${i}.png`;
+    images.push(e);
+  }
+  return images;
 };
 
 function Introduce() {
