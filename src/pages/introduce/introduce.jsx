9d82308e0ad99c9ac7cc4b725d500bd053effc17
yechuanjie <1102684711@qq.com>
2019-10-24 15:43:29 +0800

    更新样式

--- src/pages/introduce/introduce.jsx
+++ src/pages/introduce/introduce.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import './introduce.scss';
-const requireContext = require.context(
-  '../../assets/introduce',
-  true,
-  /^\.\/.*\.png$/,
-);
-const images = requireContext.keys().map(requireContext);
+// const requireContext = require.context(
+//   '../../assets/introduce',
+//   true,
+//   /^\d.\/.*\.png$/,
+// );
+// const images = requireContext.keys().map(requireContext);
+
+// 所有的表情包对象
+
+const imageList = () => {
+    const images = [];
+    for (let i = 0; i <= 10; i++) {
+      const e = require(`../../assets/introduce/${i}.png`);
+      images.push(e);
+    }
+    return images;
+};
 
 function Introduce() {
   return (
     <div className="introduce-page">
-      {images.map((image, key) => {
+      {imageList().map((image, key) => {
         return <img src={image} alt="" key={key} />;
       })}
     </div>
