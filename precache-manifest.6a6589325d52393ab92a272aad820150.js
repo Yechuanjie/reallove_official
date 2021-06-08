39dcd3990fa2aa87218a434fe34408f744a433ad
yechuanjie <1102684711@qq.com>
2021-06-08 04:37:56 +0000

    Update

---
+++ precache-manifest.6a6589325d52393ab92a272aad820150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c98712d355e66edd0c730c573dc14b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d14720562f6bcbe237db",
+    "url": "./static/css/main.3c2b83c9.chunk.css"
+  },
+  {
+    "revision": "56946b53b41850adc826",
+    "url": "./static/js/2.92f1bd52.chunk.js"
+  },
+  {
+    "revision": "d14720562f6bcbe237db",
+    "url": "./static/js/main.d2a89d64.chunk.js"
+  },
+  {
+    "revision": "da86d72c010e083270a3",
+    "url": "./static/js/runtime-main.730b2458.js"
+  },
+  {
+    "revision": "27f585fa8f1dcfe283ef74d495772d8e",
+    "url": "./static/media/comming.27f585fa.png"
+  },
+  {
+    "revision": "5a847f8e6da14639f68c00f7224dac0f",
+    "url": "./static/media/wechat-icon.5a847f8e.jpg"
+  }
+]);
